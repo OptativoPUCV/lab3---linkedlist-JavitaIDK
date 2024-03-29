42fc190144d73e76e601487e0ce0d6f35d079a33
Your Name <you@example.com>
2024-03-29 23:02:44 +0000

    new try

--- list.c
+++ list.c
@@ -114,8 +114,13 @@ void * popCurrent(List * list)
     left = left->next;
   left->next = right;
   free(list->current);
+  return list->current;
+  list->current=NULL;
   
 }
+
+  
+
 
 void cleanList(List * list) {
     while (list->head != NULL) {
